8a1ec20078dc558ea3e7fd84168902c2b370aab5
phlp <philip.woltersdorf@web.de>
2026-01-11 14:13:54 +0100

    Fix edge scrolling: use dynamic window dimensions instead of hardcoded constants

--- src/game.c
+++ src/game.c
@@ -4,10 +4,13 @@
 void Game_Update(AppState *s, float dt) {
     float move_speed = CAMERA_SPEED / s->zoom;
     
+    int win_w, win_h;
+    SDL_GetRenderOutputSize(s->renderer, &win_w, &win_h);
+
     bool scroll_left = s->mouse_pos.x < EDGE_SCROLL_THRESHOLD;
-    bool scroll_right = s->mouse_pos.x > (WINDOW_WIDTH - EDGE_SCROLL_THRESHOLD);
+    bool scroll_right = s->mouse_pos.x > (win_w - EDGE_SCROLL_THRESHOLD);
     bool scroll_up = s->mouse_pos.y < EDGE_SCROLL_THRESHOLD;
-    bool scroll_down = s->mouse_pos.y > (WINDOW_HEIGHT - EDGE_SCROLL_THRESHOLD);
+    bool scroll_down = s->mouse_pos.y > (win_h - EDGE_SCROLL_THRESHOLD);
 
     if (scroll_up)    s->camera_pos.y -= move_speed * dt;
     if (scroll_down)  s->camera_pos.y += move_speed * dt;
