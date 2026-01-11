70ca04bc8d2d0f4f4df8ccf0941a989266c0c221
phlp <philip.woltersdorf@web.de>
2026-01-11 08:37:40 +0100

    Add two-layer grid system (200x200 and 1000x1000) with world coordinate labels

--- src/renderer.c
+++ src/renderer.c
@@ -12,7 +12,6 @@ typedef struct {
 } ColorStop;
 
 static void GetNebulaColor(float t, float *r, float *g, float *b) {
-  // Brighter Dark Nebula Gradient
   static const ColorStop stops[] = {
       {0.00f, 2, 2, 6},    // Absolute Void
       {0.40f, 10, 10, 25}, // Deep Space Blue
@@ -23,15 +22,11 @@ static void GetNebulaColor(float t, float *r, float *g, float *b) {
   static const int num_stops = 5;
 
   if (t <= stops[0].pos) {
-    *r = stops[0].r;
-    *g = stops[0].g;
-    *b = stops[0].b;
+    *r = stops[0].r; *g = stops[0].g; *b = stops[0].b;
     return;
   }
   if (t >= stops[num_stops - 1].pos) {
-    *r = stops[num_stops - 1].r;
-    *g = stops[num_stops - 1].g;
-    *b = stops[num_stops - 1].b;
+    *r = stops[num_stops - 1].r; *g = stops[num_stops - 1].g; *b = stops[num_stops - 1].b;
     return;
   }
 
@@ -53,7 +48,6 @@ static int SDLCALL BackgroundGenerationThread(void *data) {
 
   while (SDL_GetAtomicInt(&s->bg_should_quit) == 0) {
     if (SDL_GetAtomicInt(&s->bg_request_update) == 1) {
-
       SDL_LockMutex(s->bg_mutex);
       Vec2 cam_pos = s->bg_target_cam_pos;
       float zoom = s->bg_target_zoom;
@@ -64,12 +58,10 @@ static int SDLCALL BackgroundGenerationThread(void *data) {
       float s_low = 0.001f;
       float s_med = 0.003f;
       float color_scale = 0.00001f;
-
       float w_macro = 0.6f;
       float w_low = 0.3f;
       float w_med = 0.1f;
 
-      // Even slower drift for almost stationary cosmic clouds
       float drift_macro_x = time * 0.05f;
       float drift_macro_y = time * 0.03f;
       float drift_detail_x = time * 0.15f;
@@ -78,62 +70,45 @@ static int SDLCALL BackgroundGenerationThread(void *data) {
       for (int i = 0; i < s->bg_w * s->bg_h; i++) {
         int x = i % s->bg_w;
         int y = i / s->bg_w;
-
         float world_x = cam_pos.x + (x * BG_SCALE_FACTOR) / zoom;
         float world_y = cam_pos.y + (y * BG_SCALE_FACTOR) / zoom;
-
-        // Apply different drift to different layers for a morphing effect
         float n = Noise2D(world_x * s_macro + drift_macro_x, world_y * s_macro + drift_macro_y) * w_macro +
                   Noise2D(world_x * s_low   + drift_detail_x, world_y * s_low   + drift_detail_y) * w_low +
                   Noise2D(world_x * s_med   + drift_detail_x, world_y * s_med   + drift_detail_y) * w_med;
-
-        float variation = Noise2D(world_x * color_scale + 12345.0f,
-                                  world_y * color_scale + 67890.0f);
-
+        float variation = Noise2D(world_x * color_scale + 12345.0f, world_y * color_scale + 67890.0f);
         if (n > 1.0f) n = 1.0f; if (n < 0.0f) n = 0.0f;
         if (variation > 1.0f) variation = 1.0f; if (variation < 0.0f) variation = 0.0f;
-
         float r_f, g_f, b_f;
         GetNebulaColor(n, &r_f, &g_f, &b_f);
-
         r_f *= (0.95f + variation * 0.1f);
         g_f *= (0.95f + (1.0f - variation) * 0.1f);
-
         float intensity = 0.5f + n * 0.5f;
         Uint8 r = (Uint8)fminf(r_f * intensity, 255.0f);
         Uint8 g = (Uint8)fminf(g_f * intensity, 255.0f);
         Uint8 b = (Uint8)fminf(b_f * intensity, 255.0f);
-
         s->bg_pixel_buffer[i] = (255 << 24) | (b << 16) | (g << 8) | r;
       }
 
-      // Blur
-      for (int y = 0; y < s->bg_h; y++) {
-        for (int x = 0; x < s->bg_w; x++) {
-          int i = y * s->bg_w + x;
-          int r_sum = 0, g_sum = 0, b_sum = 0;
-          int count = 0;
-#define ADD_PIXEL(idx) \
-  do { \
-    Uint32 p = s->bg_pixel_buffer[idx]; \
-    r_sum += (p & 0xFF); \
-    g_sum += ((p >> 8) & 0xFF); \
-    b_sum += ((p >> 16) & 0xFF); \
-    count++; \
-  } while (0)
-          ADD_PIXEL(i);
-          if (x > 0) ADD_PIXEL(i - 1);
-          if (x < s->bg_w - 1) ADD_PIXEL(i + 1);
-          if (y > 0) ADD_PIXEL(i - s->bg_w);
-          if (y < s->bg_h - 1) ADD_PIXEL(i + s->bg_w);
-#undef ADD_PIXEL
-          Uint8 r_val = r_sum / count;
-          Uint8 g_val = g_sum / count;
-          Uint8 b_val = b_sum / count;
-          s->bg_pixel_buffer[i] = (255 << 24) | (b_val << 16) | (g_val << 8) | r_val;
+      for (int pass = 0; pass < 1; pass++) {
+        for (int y = 0; y < s->bg_h; y++) {
+          for (int x = 0; x < s->bg_w; x++) {
+            int i = y * s->bg_w + x;
+            int r_sum = 0, g_sum = 0, b_sum = 0;
+            int count = 0;
+            #define ADD_PIXEL(idx) do { Uint32 p = s->bg_pixel_buffer[idx]; r_sum += (p & 0xFF); g_sum += ((p >> 8) & 0xFF); b_sum += ((p >> 16) & 0xFF); count++; } while(0)
+            ADD_PIXEL(i);
+            if (x > 0) ADD_PIXEL(i - 1);
+            if (x < s->bg_w - 1) ADD_PIXEL(i + 1);
+            if (y > 0) ADD_PIXEL(i - s->bg_w);
+            if (y < s->bg_h - 1) ADD_PIXEL(i + s->bg_w);
+            #undef ADD_PIXEL
+            Uint8 r_val = r_sum / count;
+            Uint8 g_val = g_sum / count;
+            Uint8 b_val = b_sum / count;
+            s->bg_pixel_buffer[i] = (255 << 24) | (b_val << 16) | (g_val << 8) | r_val;
+          }
         }
       }
-
       SDL_SetAtomicInt(&s->bg_request_update, 0);
       SDL_SetAtomicInt(&s->bg_data_ready, 1);
     } else {
@@ -149,21 +124,19 @@ void Renderer_Init(AppState *s) {
   s->bg_w = w / BG_SCALE_FACTOR;
   s->bg_h = h / BG_SCALE_FACTOR;
   s->bg_pixel_buffer = SDL_calloc(s->bg_w * s->bg_h, sizeof(Uint32));
-  s->bg_texture = SDL_CreateTexture(s->renderer, SDL_PIXELFORMAT_ABGR8888,
-                                    SDL_TEXTUREACCESS_STREAMING, s->bg_w, s->bg_h);
+  s->bg_texture = SDL_CreateTexture(s->renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, s->bg_w, s->bg_h);
   if (s->bg_texture) {
     SDL_SetTextureScaleMode(s->bg_texture, SDL_SCALEMODE_LINEAR);
   }
+  SDL_SetRenderDrawBlendMode(s->renderer, SDL_BLENDMODE_BLEND);
   s->bg_mutex = SDL_CreateMutex();
   SDL_SetAtomicInt(&s->bg_should_quit, 0);
   SDL_SetAtomicInt(&s->bg_request_update, 0);
   SDL_SetAtomicInt(&s->bg_data_ready, 0);
-  
   s->bg_target_cam_pos = s->camera_pos;
   s->bg_target_zoom = s->zoom;
   s->bg_target_time = s->current_time;
   SDL_SetAtomicInt(&s->bg_request_update, 1);
-
   s->bg_thread = SDL_CreateThread(BackgroundGenerationThread, "BG_Gen", s);
 }
 
@@ -194,17 +167,48 @@ static void UpdateBackground(AppState *s) {
 }
 
 static void DrawGrid(SDL_Renderer *renderer, const AppState *s, int win_w, int win_h) {
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 60);
-  int grid_size = 200;
-  int start_x = (int)floorf(s->camera_pos.x / grid_size) * grid_size;
-  int start_y = (int)floorf(s->camera_pos.y / grid_size) * grid_size;
-  for (float x = start_x; x < s->camera_pos.x + win_w / s->zoom + grid_size; x += grid_size) {
-    float screen_x = (x - s->camera_pos.x) * s->zoom;
-    SDL_RenderLine(renderer, screen_x, 0, screen_x, (float)win_h);
+  // 1. Draw Small Grid (200x200)
+  SDL_SetRenderDrawColor(renderer, 50, 50, 50, 40);
+  int grid_small = 200;
+  int start_x_s = (int)floorf(s->camera_pos.x / grid_small) * grid_small;
+  int start_y_s = (int)floorf(s->camera_pos.y / grid_small) * grid_small;
+  for (float x = start_x_s; x < s->camera_pos.x + win_w / s->zoom + grid_small; x += grid_small) {
+    float sx = (x - s->camera_pos.x) * s->zoom;
+    SDL_RenderLine(renderer, sx, 0, sx, (float)win_h);
   }
-  for (float y = start_y; y < s->camera_pos.y + win_h / s->zoom + grid_size; y += grid_size) {
-    float screen_y = (y - s->camera_pos.y) * s->zoom;
-    SDL_RenderLine(renderer, 0, screen_y, (float)win_w, screen_y);
+  for (float y = start_y_s; y < s->camera_pos.y + win_h / s->zoom + grid_small; y += grid_small) {
+    float sy = (y - s->camera_pos.y) * s->zoom;
+    SDL_RenderLine(renderer, 0, sy, (float)win_w, sy);
+  }
+
+  // 2. Draw Large Grid (1000x1000) with Labels
+  SDL_SetRenderDrawColor(renderer, 100, 100, 100, 80);
+  int grid_large = 1000;
+  int start_x_l = (int)floorf(s->camera_pos.x / grid_large) * grid_large;
+  int start_y_l = (int)floorf(s->camera_pos.y / grid_large) * grid_large;
+  
+  for (float x = start_x_l; x < s->camera_pos.x + win_w / s->zoom + grid_large; x += grid_large) {
+    float sx = (x - s->camera_pos.x) * s->zoom;
+    SDL_RenderLine(renderer, sx, 0, sx, (float)win_h);
+  }
+  for (float y = start_y_l; y < s->camera_pos.y + win_h / s->zoom + grid_large; y += grid_large) {
+    float sy = (y - s->camera_pos.y) * s->zoom;
+    SDL_RenderLine(renderer, 0, sy, (float)win_w, sy);
+  }
+
+  // 3. Draw Large Grid Labels
+  SDL_SetRenderDrawColor(renderer, 150, 150, 150, 150);
+  for (float x = start_x_l; x < s->camera_pos.x + win_w / s->zoom + grid_large; x += grid_large) {
+    for (float y = start_y_l; y < s->camera_pos.y + win_h / s->zoom + grid_large; y += grid_large) {
+        float sx = (x - s->camera_pos.x) * s->zoom;
+        float sy = (y - s->camera_pos.y) * s->zoom;
+        
+        if (sx >= -10 && sx < win_w && sy >= -10 && sy < win_h) {
+            char label[32];
+            snprintf(label, sizeof(label), "(%.0f,%.0f)", x, y);
+            SDL_RenderDebugText(renderer, sx + 5, sy + 5, label);
+        }
+    }
   }
 }
 
